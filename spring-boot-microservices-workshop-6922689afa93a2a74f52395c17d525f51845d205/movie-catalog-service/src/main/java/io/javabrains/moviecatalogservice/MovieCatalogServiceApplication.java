b3deb64a9c1322aba3e16d3f0b4ba439cb4b6b38
ANIL KUMAR REDDINENI <anilkumarreddineni@gmail.com>
2019-12-02 19:43:08 +0530

    This is just a commit to test.

--- spring-boot-microservices-workshop-6922689afa93a2a74f52395c17d525f51845d205/movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/MovieCatalogServiceApplication.java
+++ spring-boot-microservices-workshop-6922689afa93a2a74f52395c17d525f51845d205/movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -3,6 +3,11 @@ package io.javabrains.moviecatalogservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * This is the main service which the other services are being called.
+ * @author anilkumar.r
+ *
+ */
 @SpringBootApplication
 public class MovieCatalogServiceApplication {
 
